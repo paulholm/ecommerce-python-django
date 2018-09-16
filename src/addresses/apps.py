7c903ed29d6fa7a897ac0a50eb1c850d0c784264
paulholm <27723825+paulholm@users.noreply.github.com>
2018-09-16 16:21:37 +0100

    Completed django ecommerce

---
+++ src/addresses/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class AddressesConfig(AppConfig):
+    name = 'addresses'
---
+++ src/ecommerce/views.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import authenticate, login, get_user_model
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render,redirect
+
+from .forms import ContactForm
+
+def home_page(request):
+    context = {
+        "title":"Hello World!",
+        "content":" Welcome to the homepage.",
+
+    }
+    if request.user.is_authenticated():
+        context["premium_content"] = "YEAHHHHHH"
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title":"About Page",
+        "content":" Welcome to the about page."
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title":"Contact",
+        "content":" Welcome to the contact page.",
+        "form": contact_form,
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+        if request.is_ajax():
+            return JsonResponse({"message": "Thank you for your submission"})
+
+    if contact_form.errors:
+        errors = contact_form.errors.as_json()
+        if request.is_ajax():
+            return HttpResponse(errors, status=400, content_type='application/json')
+    return render(request, "contact/view.html", context)
